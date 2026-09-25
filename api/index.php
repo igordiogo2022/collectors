24db9ca379fc603b8aa3ada2daa146256e761127
Igor Diogo <igordiogo1314@gmail.com>
2026-09-24 22:14:14 -0300

    Ajuste

--- api/index.php
+++ api/index.php
@@ -8,8 +8,8 @@ require_once 'dados.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="img\collectors.png" type="image/x-icon">
-    <link rel="stylesheet" href="css\style.css">
+    <link rel="shortcut icon" href="/img/collectors.png" type="image/x-icon">
+    <link rel="stylesheet" href="..\css\style.css">
     <title>Título da Página</title>
 </head>
 <body>
