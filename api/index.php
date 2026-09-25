585597bdec1381fd808595a5972db80b9bfd0bda
Igor Diogo <igordiogo1314@gmail.com>
2026-09-24 22:50:50 -0300

    Ajuste

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'dados.php';
+require_once __DIR__ . '/../dados.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,12 +9,12 @@ require_once 'dados.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="shortcut icon" href="/img/collectors.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Título da Página</title>
 </head>
 <body>
     <?php 
-    include 'header.php'; 
+    require_once __DIR__ . '/../header.php';
     ?>
     <section id="carrossel" class="carousel slide"> 
         <div class="carousel-inner">
